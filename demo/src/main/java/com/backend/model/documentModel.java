68597a5520bdaf5fc6c1d4cbf93a041a7b730134
nayanbecker <nayan@unochapeco.edu.br>
2025-07-30 21:21:25 +0000

    documentModel

---
+++ demo/src/main/java/com/backend/model/documentModel.java
@@ -0,0 +1,14 @@
+package com.backend.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class documentModel {
+
+    private String documentConfig;
+    private String documentContent;
+}
